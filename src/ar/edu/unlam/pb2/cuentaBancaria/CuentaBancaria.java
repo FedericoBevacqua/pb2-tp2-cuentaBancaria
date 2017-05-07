280cba377fbba7f9642ac7be5a84119eea67c7bf
Federico Bevacqua <fede.el.xeneize@gmail.com>
2017-05-07 02:50:18 -0300

    Actualizada clase CuentaBancaria.

--- src/ar/edu/unlam/pb2/cuentaBancaria/CuentaBancaria.java
+++ src/ar/edu/unlam/pb2/cuentaBancaria/CuentaBancaria.java
@@ -1,7 +1,34 @@
 package ar.edu.unlam.pb2.cuentaBancaria;
 
 public class CuentaBancaria {
-	
-	
+	private Double saldo;
+	private Integer cantidadExtracciones;
+
+	public CuentaBancaria() {
+		super();
+		this.saldo = 0D;
+		this.cantidadExtracciones = 0;
+	}
+
+	public void depositar(Double monto) {
+		Double saldoActual = this.getSaldo();
+		this.setSaldo(saldoActual + monto);
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	protected void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+
+	public Integer getCantidadExtracciones() {
+		return cantidadExtracciones;
+	}
+
+	protected void setCantidadExtracciones(Integer cantidadExtracciones) {
+		this.cantidadExtracciones = cantidadExtracciones;
+	}
 
 }
